7fc9aff7dc4487a680ac0c74a5f5a98f5415414b
Jared Harvick <jharvick38@gmail.com>
2023-03-19 14:47:13 -0500

    Added User, Item, and Favorite models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# User.create(name: "Jared", email: "jared@test.com", password_digest: "password", image_url: "https://static.wikia.nocookie.net/p__/images/3/36/Farvarod_08.png/revision/latest?cb=20190728143717&path-prefix=protagonist")
+# User.create(name: "Chloe", email: "chloe@test.com", password_digest: "password", image_url: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2018%2F02%2F2475_tp_00013r-2000.jpg")
+
+# Item.create(name: "Anchorman: The Legend of Ron Burgandy", image_url: "https://thumbs.gfycat.com/FrighteningEmbellishedFeline-max-1mb.gif", description: "The funniest movie of all time.", c)
