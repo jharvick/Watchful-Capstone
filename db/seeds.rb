0f50ce88cc7b7f2e02316e38cd9092b18d6d1fae
Jared Harvick <jharvick38@gmail.com>
2023-03-22 21:51:46 -0500

    Add favorites in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(name: "Jared", email: "jared@test.com", password: "password", image_url: "https://static.wikia.nocookie.net/p__/images/3/36/Farvarod_08.png/revision/latest?cb=20190728143717&path-prefix=protagonist")
-User.create(name: "Chloe", email: "chloe@test.com", password: "password", image_url: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2018%2F02%2F2475_tp_00013r-2000.jpg")
 
-Item.create(name: "Anchorman: The Legend of Ron Burgandy", image_url: "https://thumbs.gfycat.com/FrighteningEmbellishedFeline-max-1mb.gif", description: "The funniest movie of all time.", category: "Movie")
-Item.create(name: "How to be Single", image_url: "https://thumbs.gfycat.com/FrighteningEmbellishedFeline-max-1mb.gif", description: "The funniest movie of all time.", category: "Movie")
+# Favorite.create(user_id: 1, item_id: 1)
+# Favorite.create(user_id: 2, item_id: 2)
+
+# User.create(name: "Jared", email: "jared@test.com", password: "password", image_url: "https://static.wikia.nocookie.net/p__/images/3/36/Farvarod_08.png/revision/latest?cb=20190728143717&path-prefix=protagonist")
+# User.create(name: "Chloe", email: "chloe@test.com", password: "password", image_url: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2018%2F02%2F2475_tp_00013r-2000.jpg")
+
+# Item.create(name: "Anchorman: The Legend of Ron Burgandy", image_url: "https://thumbs.gfycat.com/FrighteningEmbellishedFeline-max-1mb.gif", description: "The funniest movie of all time.", category: "Movie")
+# Item.create(name: "How to be Single", image_url: "https://thumbs.gfycat.com/FrighteningEmbellishedFeline-max-1mb.gif", description: "The funniest movie of all time.", category: "Movie")
